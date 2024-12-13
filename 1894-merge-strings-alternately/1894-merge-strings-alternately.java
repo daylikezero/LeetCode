3bbbcd709e8c093b8f1c9b7676a1ea8c60504360
daylikezero <daylikezero@gmail.com>
2024-12-13 16:07:16 +0900

    Time: 1 ms (83.66%), Space: 41.7 MB (54.36%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,23 +1,19 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String result = "";
+        // 1. String -> StringBuffer
+        StringBuffer result = new StringBuffer();
         int word1Length = word1.length();
         int word2Length = word2.length();
         int max = Math.max(word1Length, word2Length);
-        if (word1Length > word2Length) {
-            for (int i = word2Length; i < word1Length; i++) {
-                word2 += " ";
-            }
-        } else if (word1Length < word2Length) {
-            for (int i = word1Length; i < word2Length; i++) {
-                word1 += " ";
-            }
-        }
+        // 2. word 의 길이와 현재 index 비교
         for (int i = 0; i < max; i++) {
-            result += word1.charAt(i);
-            result += word2.charAt(i);
+            if (i < word1Length) {
+                result.append(word1.charAt(i));
+            }
+            if (i < word2Length) {
+                result.append(word2.charAt(i));
+            }
         }
-        result = result.replaceAll(" ", "");
-        return result;
+        return result.toString();
     }
 }
